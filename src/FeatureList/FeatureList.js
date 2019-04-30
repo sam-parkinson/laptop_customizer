56ffbde0a92953a9316355ae7af274e5b9565fa6
Sam Parkinson <parkinsonsp42@gmail.com>
2019-04-30 13:18:46 -0400

    update react app

---
+++ src/FeatureList/FeatureList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './FeatureList.css';
+
+class FeatureList extends Component {
+
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default FeatureList
