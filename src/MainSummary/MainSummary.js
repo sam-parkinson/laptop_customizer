57c9d75a597dca5f8184581565040f269b2e9405
Sam Parkinson <parkinsonsp42@gmail.com>
2019-05-01 17:04:13 -0400

    Add TotalPrice component

--- src/MainSummary/MainSummary.js
+++ src/MainSummary/MainSummary.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './MainSummary.css';
 import SummaryItem from '../SummaryItem/SummaryItem';
+import TotalPrice from '../TotalPrice/TotalPrice';
 
 class MainSummary extends Component {
 
@@ -9,22 +10,13 @@ class MainSummary extends Component {
     const summary = Object.keys(this.props.selected)
           .map(key => 
              <SummaryItem key={key} selected={this.props.selected[key]}/>
-            )
-
-    const total = Object.keys(this.props.selected)
-          .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0);      
+            )     
 
     return (
       <section className="main__summary">
         <h3>NEW GREENLEAF 2018</h3>
         {summary}
-        <div className="summary__total">
-          <div className="summary__total__label">Your Price: </div>
-          <div className="summary__total__value">
-          { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-              .format(total) }
-          </div>
-        </div>
+        <TotalPrice selected={this.props.selected} />        
       </section>
     );
   }
